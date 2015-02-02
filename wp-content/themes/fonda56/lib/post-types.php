0fe20eec13a4390127d6062bbfe430ca3c266233
Federico Vezzoli <federico@dot-agency.it>
2015-02-02 11:00:48 +0100

    aggiunto post type punto vendita

--- wp-content/themes/fonda56/lib/post-types.php
+++ wp-content/themes/fonda56/lib/post-types.php
@@ -46,6 +46,49 @@ function custom_post_type() {
 	);
 	register_post_type( 'offerta', $args );
 
+
+	$labels = array(
+		'name'                => _x( 'Punti vendita', 'Post Type General Name', 'roots' ),
+		'singular_name'       => _x( 'Punto vendita', 'Post Type Singular Name', 'roots' ),
+		'menu_name'           => __( 'Punti vendita', 'roots' ),
+		'parent_item_colon'   => __( 'Punti vendita:', 'roots' ),
+		'all_items'           => __( 'Tutti punti vendita', 'roots' ),
+		'view_item'           => __( 'Vedi punto vendita', 'roots' ),
+		'add_new_item'        => __( 'Aggiungi punto vendita', 'roots' ),
+		'add_new'             => __( 'Aggiungi nuovo punto vendita', 'roots' ),
+		'edit_item'           => __( 'Modifica punto vendita', 'roots' ),
+		'update_item'         => __( 'Aggiorna punto vendita', 'roots' ),
+		'search_items'        => __( 'Cerca punto vendita', 'roots' ),
+		'not_found'           => __( 'Non trovato', 'roots' ),
+		'not_found_in_trash'  => __( 'Non trovato nel cestino', 'roots' ),
+	);
+	$rewrite = array(
+		'slug'                => 'punto-vendita',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'Punto vendita', 'roots' ),
+		'description'         => __( 'punti vendita', 'roots' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => false,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'punti-vendita', $args );
+
 }
 
 // Hook into the 'init' action
