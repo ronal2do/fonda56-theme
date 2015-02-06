ea81d4221f489d6762db921f99131ca0a0bf52a5
Federico Vezzoli <federico@dot-agency.it>
2015-02-06 10:13:11 +0100

    fixes+ adding an ajax request for the meta

--- wp-content/themes/fonda56/lib/custom.php
+++ wp-content/themes/fonda56/lib/custom.php
@@ -4,5 +4,39 @@
  */
 
  add_filter( 'json_serve_request', function( ) {
-	//header( "Access-Control-Allow-Origin: *" );
+
+	if(strpos($_SERVER['REQUEST_URI'], 'meta') !== false) {
+		//header('Access-Control-Allow-Origin: http://localhost:8100');
+	} else {
+		//header('Access-Control-Allow-Origin: http://localhost:8100');
+	}
+	header('Access-Control-Allow-Origin: *');
+	//header('Access-Control-Allow-Methods: GET, OPTIONS');
+	header('Access-Control-Allow-Credentials: true');
+	header('Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS');
+
 });
+
+
+//ajax to clear the meta
+add_action( 'wp_ajax_retrieve_meta', 'retrieve_meta');
+add_action( 'wp_ajax_nopriv_retrieve_meta', 'retrieve_meta');
+
+
+//retrieve the meta for a post
+function retrieve_meta() {
+
+	$post_id   = intval( $_REQUEST['post_id'] );
+
+	try {
+
+		echo true;
+	}
+
+	catch (Exception $e) {
+		echo false;
+	}
+
+	// chiudo l'esecuzione della chiamata AJAX, evitando di far ritornare errori da parte di WordPress
+	die();
+}
