a014321a3c51236fff970b7d204d0f152af391c0
Federico Vezzoli <federico@dot-agency.it>
2015-02-06 10:15:05 +0100

    fix

--- wp-content/themes/fonda56/lib/custom.php
+++ wp-content/themes/fonda56/lib/custom.php
@@ -12,8 +12,8 @@
 	}
 	header('Access-Control-Allow-Origin: *');
 	//header('Access-Control-Allow-Methods: GET, OPTIONS');
-	header('Access-Control-Allow-Credentials: true');
-	header('Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS');
+	//header('Access-Control-Allow-Credentials: true');
+	//header('Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS');
 
 });
 
